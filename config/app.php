a424178caf44977a55011f6dc4175610bd4143bc
swidodo <smtwidodo04@gmail.com>
2024-03-30 07:42:48 +0700

    update

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://103.190.28.59/dms'),
 
     'asset_url' => env('ASSET_URL', null),
 
