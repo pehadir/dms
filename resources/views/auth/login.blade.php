4512b5d3f23f56238ec5801d09326b79fbe5cefa
swidodo <smtwidodo04@gmail.com>
2024-03-31 17:03:15 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,7 +50,7 @@
 							<div class="d-flex justify-content-center mt-3 mb-3">
 								<img src="{{asset('assets/img/images.png')}}" width="150" alt="pehadir">
 							</div>
-							<h4 class="account-title">DOCUMENT MANAGEMENT SYSTEM</h4>
+							<h4>DOCUMENT MANAGEMENT SYSTEM</h4>
                             @if (Session::get('failed'))
                                 <div class="alert alert-info alert-dismissible fade show" role="alert">
                                     <strong>{{Session::get('failed')}}</strong>
